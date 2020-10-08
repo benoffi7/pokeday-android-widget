83c3b4fb4b65aae1fa25413e85b666bba55a114d
Facundo Rodriguez Arceri <frodriguez.arceri@gmail.com>
2020-10-07 22:45:10 -0300

    Refactors simples para eliminar warnings y corregir algunos estilos del código

--- app/src/main/java/com/coffeeandcookies/pokemondaywidget/MyAppWidgetProvider.kt
+++ app/src/main/java/com/coffeeandcookies/pokemondaywidget/MyAppWidgetProvider.kt
@@ -3,19 +3,17 @@ package com.coffeeandcookies.pokemondaywidget
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.graphics.Bitmap
 import android.util.Log
 import android.widget.RemoteViews
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.AppWidgetTarget
-import com.bumptech.glide.request.transition.Transition
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
 
-class MyAppWidgetProvider() : AppWidgetProvider() {
+class MyAppWidgetProvider : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -26,7 +24,7 @@ class MyAppWidgetProvider() : AppWidgetProvider() {
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
-            val views: RemoteViews = RemoteViews(context.packageName, R.layout.layout_widget_home)
+            val views = RemoteViews(context.packageName, R.layout.layout_widget_home)
 
             getPokemonDay(views, appWidgetManager, appWidgetId, context)
 
@@ -35,11 +33,12 @@ class MyAppWidgetProvider() : AppWidgetProvider() {
         }
     }
 
-
     private fun getPokemonDay(
-        views: RemoteViews, appWidgetManager: AppWidgetManager, appWidgetId: Int, context: Context
-    )
-    {
+        views: RemoteViews,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        context: Context
+    ) {
         val calendar: Calendar = Calendar.getInstance()
         val day: Int = calendar.get(Calendar.DAY_OF_YEAR)
 
@@ -51,39 +50,33 @@ class MyAppWidgetProvider() : AppWidgetProvider() {
                 if (response.isSuccessful) {
                     Log.d("pokemonapi", "Pokémon del día \n${response.body().toString()}")
 
-                    val text =
-                        "Pokémon del día \n #${response.body()?.id} - ${response.body()?.name}"
+                    val text = "Pokémon del día \n #${response.body()?.id} - ${response.body()?.name}"
 
                     views.setTextViewText(R.id.textView_pokemon_name, text)
 
-                    val awt: AppWidgetTarget = object : AppWidgetTarget(
+                    val awt = object : AppWidgetTarget(
                         context.applicationContext,
-                        R.id.imageView_pokemon_front, views, appWidgetId
-                    ) {
-                        override fun onResourceReady(
-                            resource: Bitmap,
-                            transition: Transition<in Bitmap>?
-                        ) {
-                            super.onResourceReady(resource, transition)
-                        }
-                    }
+                        R.id.imageView_pokemon_front,
+                        views,
+                        appWidgetId
+                    ) {}
 
-                    var options =
-                        RequestOptions().override(700, 700).placeholder(R.mipmap.ic_launcher).error(
-                            R.mipmap.ic_launcher
-                        )
+                    val options = RequestOptions()
+                        .override(700, 700)
+                        .placeholder(R.mipmap.ic_launcher)
+                        .error(R.mipmap.ic_launcher)
 
-                    Glide.with(context.applicationContext).asBitmap()
-                        .load(response.body()?.sprites?.front_default).apply(
-                        options
-                    ).into(awt)
+                    Glide.with(context.applicationContext)
+                        .asBitmap()
+                        .load(response.body()?.sprites?.front_default)
+                        .apply(options)
+                        .into(awt)
 
                     appWidgetManager.updateAppWidget(appWidgetId, views)
                 }
             }
 
-            override fun onFailure(call: Call<Pokemon>, t: Throwable) {
-            }
+            override fun onFailure(call: Call<Pokemon>, t: Throwable) {}
         })
     }
 }
